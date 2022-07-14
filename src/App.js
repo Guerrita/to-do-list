3b6c12b08de55267b7018b1fc59d68d17649ec98
Andres Guerra Montoya <aamontoya@eafit.edu.co>
2022-07-14 17:57:29 -0500

    Created structure of the todo app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+const todos = [
+  {
+    text: "Cortar cebolla",
+    completed: false,
+  },
+  {
+    text: "Tomar curso intro a React",
+    completed: false,
+  },
+  {
+    text: "Llorar",
+    completed: false,
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Modal>
+      <TodoCounter />
+      <h2>Has completado 2 de 3 TODOs</h2>
+      <TodoSearch />
+      <input placeholder="Cebolla" />
+      <TodoList>
+        {todos.map((todo) => (
+          <TodoItem todo={todo} />
+        ))}
+      </TodoList>
+      <CreateTodoButton />
+      <button>+</button>
+    </Modal>
   );
 }
 
